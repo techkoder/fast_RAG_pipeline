abde70977ba96da1253b3e2ac2f0e66087c62a37
techkoder <vinaykjain333@gmail.com>
2025-07-31 21:43:26 +0530

    updated API to use multiThreading

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI, Depends
 from app.models.schema import RunRequest, RunResponse
 from app.services.pdf_utils import download_pdf_text
@@ -10,11 +11,11 @@ app = FastAPI()
 async def hackrx_run(request: RunRequest, _auth=Depends(verify_bearer_token)):
     text = download_pdf_text(request.documents)
     chain = build_rag_chain(text)
-    answers = []
-    for q in request.questions:
+    async def ask_question(q: str):
         optimized_query = f"Pls give a precise answer in a single line. {q}"
-        result =await chain.ainvoke({"query": optimized_query})
-        answers.append(result["result"])
+        result = await chain.ainvoke({"query": optimized_query})
+        return result["result"]
+    answers = await asyncio.gather(*(ask_question(q) for q in request.questions))
     print("answers:", answers)
     return RunResponse(answers=answers)
 
